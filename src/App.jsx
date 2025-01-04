d6882d92a61b3ff2969eb0e0bc6d2f6d79cbe1db
aydinmfatih <aydinmfatih@gmail.com>
2025-01-04 20:20:02 +0300

    add API endpoints and integrate axios for book management

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import BookCreate from "./Components/BookCreate";
 import BookList from "./Components/BookList";
+import axios from "axios";
 
 function App() {
   const [books, setBooks] = useState([]);
+
+  const fetchBooks = async () => {
+    const response = await axios.get("http://localhost:3001/books");
+    setBooks(response.data);
+  }
+
+  useEffect(() => {
+    fetchBooks();
+  }, [ ]);
+
 
   const deleteBookById = (id) => {
     const updatedBooks = books.filter((book) => {
@@ -11,29 +22,39 @@ function App() {
     });
     setBooks(updatedBooks);
   };
-  const updateBookById = (id,title) => {
-     const updatedBooks = books.map((book) => {
+  const updateBookById = (id, title) => {
+    const updatedBooks = books.map((book) => {
       if (book.id === id) {
-        return {...book, title: title};
+        return { ...book, title: title };
       }
       return book;
     });
     setBooks(updatedBooks);
   };
 
-  const handleCreateBook = (title) => {
+  const createBook = async (title) => {
+   const response = await axios.post("http://localhost:3001/books", {
+      title: title
+    });
+    
+    console.log(response);
+
     setBooks([
       ...books,
-      { id: Math.round(Math.random() * 9999), title: title },
+      response.data,
     ]);
-    console.log(books);
+    
   };
 
   return (
     <div className="app">
       <h1>Book List</h1>
-      <BookList onUpdate={updateBookById} onDelete={deleteBookById} books={books} />
-      <BookCreate onSubmit={handleCreateBook} />
+      <BookList
+        onUpdate={updateBookById}
+        onDelete={deleteBookById}
+        books={books}
+      />
+      <BookCreate onSubmit={createBook} />
     </div>
   );
 }
